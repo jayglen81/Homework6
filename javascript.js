543f46f22915bc1b6ad62cb15ddce7b8a9c0885e
Jason_Harris <jayglen81@hotmail.com>
2019-09-10 18:47:28 -0700

    second push

--- javascript.js
+++ javascript.js
@@ -1,9 +1,21 @@
 $(function(){
+    populateButtons(searchArray,'searchButton','#buttonsArea');
     console.log("Page Loaded");
 })
 
 var searchArray = ["Dog", "Cat", "Bird"];
 
 function populateButtons(searchArray,classToAdd,areaToAddTo){
-    
+    $(areaToAddTo).empty();
+    for(var i=0; i<searchArray.length;i++){
+        var a = $('<button>');
+        a.addClass(classToAdd);
+        a.attr('data-type', searchArray[i]);
+        a.text(searchArray[i]);
+        $(areaToAddTo).append(a);
+    }
+}
+$(document).on('click', 'searchButton',function(){
+    var type = $(this).data('type');
+    var queryURL = "http://aoi.giphy.com/v1/gifs/search?q="+type+"&api_key=dc6zat0xFJmzCd&limit=10"
 }
